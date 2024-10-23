384182836472b6224cf31c789d26fc9db2783062
asadamatic <asadamatic@gmail.com>
2024-10-23 12:39:53 +0500

    Made the zipcode and card holder name optional for IOS.

--- ios/Classes/SwiftAuthorizeNetPlugin.swift
+++ ios/Classes/SwiftAuthorizeNetPlugin.swift
@@ -18,7 +18,6 @@ public class SwiftAuthorizeNetPlugin: NSObject, FlutterPlugin {
         guard let expiration_month = args["expiration_month"] else { result(argErr); return }
         guard let expiration_year = args["expiration_year"] else { result(argErr); return }
         guard let card_cvv = args["card_cvv"] else { result(argErr); return }
-        guard let zip_code = args["zip_code"] else { result(argErr); return }
         guard let card_holder_name = args["card_holder_name"] else { result(argErr); return }
         guard let api_login_id = args["api_login_id"] else { result(argErr); return }
         guard let client_id = args["client_id"] else { result(argErr); return }
@@ -29,14 +28,18 @@ public class SwiftAuthorizeNetPlugin: NSObject, FlutterPlugin {
         }
 
         let request = AcceptSDKRequest()
-             request.merchantAuthentication.name = api_login_id
-             request.merchantAuthentication.clientKey = client_id
-             request.securePaymentContainerRequest.webCheckOutDataType.token.cardNumber = card_number
-             request.securePaymentContainerRequest.webCheckOutDataType.token.expirationMonth = expiration_month
-             request.securePaymentContainerRequest.webCheckOutDataType.token.expirationYear = expiration_year
-             request.securePaymentContainerRequest.webCheckOutDataType.token.cardCode = card_cvv
+        request.merchantAuthentication.name = api_login_id
+        request.merchantAuthentication.clientKey = client_id
+        request.securePaymentContainerRequest.webCheckOutDataType.token.cardNumber = card_number
+        request.securePaymentContainerRequest.webCheckOutDataType.token.expirationMonth = expiration_month
+        request.securePaymentContainerRequest.webCheckOutDataType.token.expirationYear = expiration_year
+        request.securePaymentContainerRequest.webCheckOutDataType.token.cardCode = card_cvv
         request.securePaymentContainerRequest.webCheckOutDataType.token.fullName = card_holder_name
-        request.securePaymentContainerRequest.webCheckOutDataType.token.zip = zip_code
+        
+        // Only set zip code if it exists in the arguments
+        if let zip_code = args["zip_code"], !zip_code.isEmpty {
+            request.securePaymentContainerRequest.webCheckOutDataType.token.zip = zip_code
+        }
         
         handler!.getTokenWithRequest(request, successHandler: { (inResponse:AcceptSDKTokenResponse) -> () in
             DispatchQueue.main.async {
