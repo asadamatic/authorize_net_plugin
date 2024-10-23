706115696c84c7154bfe6cafe6de25896b521119
asadamatic <asadamatic@gmail.com>
2024-10-23 12:22:49 +0500

    Made the zipcode and card holder name optional.

--- lib/authorize_net_plugin.dart
+++ lib/authorize_net_plugin.dart
@@ -3,19 +3,21 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class AuthorizeNetPlugin {
-  static const MethodChannel _channel = const MethodChannel('authorize_net_plugin');
+  static const MethodChannel _channel =
+      const MethodChannel('authorize_net_plugin');
 
   static Future<String> authorizeNetToken(
-      {required env,
-      required cardNumber,
-      required expirationMonth,
-      required expirationYear,
-      required cardCvv,
-      required zipCode,
-      required cardHolderName,
-      required apiLoginId,
-      required clientId}) async {
-    final String version = await _channel.invokeMethod('authorizeNetToken', <String, String>{
+      {required String env,
+      required String cardNumber,
+      required String expirationMonth,
+      required String expirationYear,
+      required String cardCvv,
+      String? zipCode,
+      required String cardHolderName,
+      required String apiLoginId,
+      required String clientId}) async {
+    final String version =
+        await _channel.invokeMethod('authorizeNetToken', <String, String?>{
       'env': env,
       'card_number': cardNumber,
       'expiration_month': expirationMonth,
